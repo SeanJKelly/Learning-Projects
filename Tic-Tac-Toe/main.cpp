8e86ed34910b03a6746a93e47c555bb0a5a5d216
SeanJKelly <seanjosephkelly14@hotmail.com>
2015-12-23 13:26:14 -0500

    Changed player input when picking spot
    No longer will you have to pick a row and then a column, you can just choose a number from the board supplied to you.

--- Tic-Tac-Toe/main.cpp
+++ Tic-Tac-Toe/main.cpp
@@ -9,7 +9,7 @@
  * example: [0][1] of an array with 4 elements (2x2), would be:
  *          (0 * 2) + 1 = index of 3, which would point to the 2nd element!
  */
-int getIndexInArray(int amountOfColumns, int rowToCheck, int columnToCheck)
+int convertMultiToSingleDimension(int amountOfColumns, int rowToCheck, int columnToCheck)
 {
     return (rowToCheck * amountOfColumns) + columnToCheck;
 }
@@ -23,7 +23,17 @@ void drawBoard(char *board, int amountOfRows, int amountOfColumns)
     {
         for (int column = 0; column < amountOfColumns; ++column)
         {
-            cout << board[currentElement++]; //Check the current element before increasing it
+            // Only display a number if there is nothing already there, such as an 'X'
+            if (board[currentElement] != 'X' && board[currentElement] != 'O')
+            {
+                cout << ((currentElement++) + 1);
+            }
+            else
+            {
+                //we output the current element before increasing the index
+                cout << board[currentElement++];
+            }
+
             // We don't add a spacer after the last element in each row
             if (column != (amountOfColumns - 1))
             {
@@ -39,24 +49,40 @@ void placeSymbol(char *board, int amountOfRows, int amountOfColumns, players::Pl
 {
     using namespace std;
 
-    short row;
-    short column;
-    int indexOfArray;
+    int indexOfArray{ 0 };
     char elementInArray = 0;
     bool continueLoop = true;
 
     do
     {
-        cout << "Which row would you like to place your marker in? ";
-        cin >> row;
+        cout << "Which spot would you like to place your marker in: ";
+        short spotToPlaceIn;
+        cin >> spotToPlaceIn;
 
-        cout << "Which column would you like to place your marker in? ";
-        cin >> column;
+        // Converts the single dimensional index they picked to
+        // a multiple dimensional index to be used throughout the program
+        short row{ 0 };
+        short column{ 0 };
+        int currentElement{ 0 };
+        for (int currentRow = 0; currentRow < amountOfRows; ++currentRow)
+        {
+            for (int correntColumn = 0; correntColumn < amountOfColumns; ++correntColumn)
+            {
+                // We subtract spotToPlaceIn on account arrays start with 0 not 1
+                // check currentElement before we increase
+                if ((spotToPlaceIn - 1) == currentElement++)
+                {
+                    row = currentRow;
+                    column = correntColumn;
+                    break;
+                }
+            }
+        }
 
         // Check to see if the selection is not out of bounds
         if (row < amountOfRows && column < amountOfColumns)
         {
-            indexOfArray = getIndexInArray(amountOfColumns, row, column);
+            indexOfArray = convertMultiToSingleDimension(amountOfColumns, row, column);
             elementInArray = board[indexOfArray];
             continueLoop = false;
             if (elementInArray == 'X' || elementInArray == 'O')
@@ -84,13 +110,12 @@ bool checkRows(char *board, int amountOfRows, int amountOfColumns, players::Play
         int amountOfWinningCharacters{ 0 };
         for (int column = 0; column < amountOfColumns; ++column)
         {
-            int indexOfCurrentElement = getIndexInArray(amountOfColumns, row, column);
+            int indexOfCurrentElement = convertMultiToSingleDimension(amountOfColumns, row, column);
 
             if (board[indexOfCurrentElement] == playerToSymbol(currentPlayer))
                 ++amountOfWinningCharacters;
         }
-        // We check columns on account a row is located on seperate columns 
-        // rows are NOT on rows
+        // We check columns on account a row is located on seperate columns rows are NOT on rows
         if (amountOfWinningCharacters == amountOfColumns)
             return true;
     }
@@ -105,7 +130,7 @@ bool checkColumns(char *board, int amountOfRows, int amountOfColumns, players::P
         int amountOfWinningCharacters{ 0 };
         for (int row = 0; row < amountOfRows; ++row)
         {
-            int indexOfCurrentElement = getIndexInArray(amountOfColumns, row, column);
+            int indexOfCurrentElement = convertMultiToSingleDimension(amountOfColumns, row, column);
 
             if (board[indexOfCurrentElement] == playerToSymbol(currentPlayer))
                 ++amountOfWinningCharacters;
@@ -118,6 +143,7 @@ bool checkColumns(char *board, int amountOfRows, int amountOfColumns, players::P
     return false;
 }
 
+// Checks diagonals for any winning conditions
 bool checkDiagonals(char *board, int amountOfRows, int amountOfColumns, players::Players currentPlayer)
 {
     // 0,0 | 0,1 | 0,2
@@ -129,7 +155,7 @@ bool checkDiagonals(char *board, int amountOfRows, int amountOfColumns, players:
     bool gameIsOver = false;
     for (int row = 0, column = 0; row < amountOfRows; ++row, ++column)
     {
-        int indexOfCurrentElement = getIndexInArray(amountOfColumns, row, column);
+        int indexOfCurrentElement = convertMultiToSingleDimension(amountOfColumns, row, column);
         if (board[indexOfCurrentElement] != playerToSymbol(currentPlayer))
         {
             gameIsOver = false;
@@ -150,7 +176,7 @@ bool checkDiagonals(char *board, int amountOfRows, int amountOfColumns, players:
     // Move the direction of the check from the bottom left to the top right
     for (int row = 2, column = 0; column < amountOfColumns; --row, ++column)
     {
-        int indexOfCurrentElement = getIndexInArray(amountOfColumns, row, column);
+        int indexOfCurrentElement = convertMultiToSingleDimension(amountOfColumns, row, column);
         if (board[indexOfCurrentElement] != playerToSymbol(currentPlayer))
         {
             gameIsOver = false;
@@ -173,9 +199,9 @@ bool checkDiagonals(char *board, int amountOfRows, int amountOfColumns, players:
     }
 }
 
-// Checks rows / columns / and diagonals for winning conditions
-// TRUE  = someone has won / tied
-// FALSE = no game ending conditions found
+/* Used as a medium for the three functions above, also handles whether to check for
+ * diagonals or not.
+ */
 bool checkWinningCondition(char *board, int amountOfRows, int amountOfColumns, players::Players currentPlayer)
 {
     bool diagonalWithWinningCondition = false;
@@ -198,7 +224,7 @@ bool checkWinningCondition(char *board, int amountOfRows, int amountOfColumns, p
     }
 }
 
-// Asks players if they wish to play, return TRUE if they do, if not return FALSE
+// Asks players if they wish to play again, return TRUE if they do, if not return FALSE
 bool requestReplay()
 {
     std::cout << "Would you like to play again? Y/N";
@@ -265,30 +291,31 @@ bool gameMain()
         if ((turn % 2) == 1) currentPlayer = players::PLAYER_ONE;
         else                 currentPlayer = players::PLAYER_TWO;
 
-        placeSymbol(board, rows, columns, currentPlayer);
         drawBoard(board, rows, columns);
+        placeSymbol(board, rows, columns, currentPlayer);
         if (checkWinningCondition(board, rows, columns, currentPlayer))
         {
+            // We are done with the board de-allocate it
+            delete board;
+            board = 0;
             return true;
         }
     }
-    //std::cout << "Game has ended in a tie\n";
+    std::cout << "Game is a tie\n";
+
+    // We are done with the board de-allocate it
+    delete board;
+    board = 0;
     return true;
 }
 
-
 int main()
 {
-    gameMain();
-    /*
-
     do
     {
         gameMain(); 
     } 
     while (requestReplay());
 
-    */
     return 0;
 }
-
